6e2541bee9bad116d43760918b531e42ccfc8b3a
Rainer <rainer@software-architects.at>
2023-12-03 10:21:27 +0100

    Slight enhancement

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -5,7 +5,7 @@ var lines = File.ReadAllLines("data.txt");
 var specialCharsCoords = lines
     .SelectMany((line, y) => line
         .Select((c, x) => (c, x))
-        .Where(t => "*&/@=+#$%-".Contains(t.c))
+        .Where(t => !char.IsDigit(t.c) && t.c != '.')
         .Select(t => new SpecialChar(t.c, t.x, y)))
     .ToArray();
 
@@ -15,7 +15,8 @@ var numbers = lines
     .ToArray();
 Console.WriteLine(numbers.Where(n => specialCharsCoords.Any(s => s.IsInRect(n))).Sum(n => n.Value));
 
-Console.WriteLine(specialCharsCoords.Where(c => c.C == '*')
+Console.WriteLine(specialCharsCoords
+    .Where(c => c.C == '*')
     .Select(c => numbers.Where(n => c.IsInRect(n)))
     .Where(c => c.Count() == 2)
     .Sum(c => c.Aggregate(1, (acc, n) => acc * n.Value)));
